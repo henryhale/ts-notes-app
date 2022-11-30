53e5fe5fef38d3bddf6c57b56240a140a16b09f4
henryhale <henryharrison8117@gmail.com>
2022-11-30 12:23:34 -0600

    demo

--- src/helpers.ts
+++
@@ -1,8 +0,0 @@
-import { GetElemId } from "./interfaces";
-
-/**
- * Get element by `id`
- * @param id 
- * @returns 
- */
-export const getElemId: GetElemId = (id: string) => <HTMLDivElement>document.getElementById(id)
